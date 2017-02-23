49899954afdc37f828557ea10fd3d9643e60116b
Bryle Castro <b2castro@ucsd.edu>
2017-02-22 23:47:48 -0800

    google analytics

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var projects = require('../projects.json');
 /*
  * GET home page.
  */
+exports.viewMin = function(req, res){
+  	res.render('index', projects);
+  	 project["min"] = true;
+};
 
 exports.view = function(req, res){
   	res.render('index', projects);
+    projects["min"] = false;
 };
